da44da89d9a8ae2ace328d7dbc5b41e4567c7403
moalshaaban <66526800+moalshaaban@users.noreply.github.com>
2020-06-13 22:27:17 +0300

    تمرين الاول

---
+++ تمرين الاول/CW-1-mohammed alshaaban.playground/Contents.swift
@@ -0,0 +1,26 @@
+
+let name = "محمد الشعبان"
+
+var age = 15
+var height = 1.7
+var hobby1 = "Running"
+var hobby2 = "Swimming"
+
+print("my name is: ", name, ", and my age is \(age), and my height is: \(height), my hobbies are: \(hobby1),\(hobby2)")
+
+//my friend
+let friendname = "Ahmed"
+
+var friendHeight = 1.68
+var friendAge = 16
+var friendHobby1 = "Running"
+var friendHobby2 = "Cooking"
+
+var age_difference = age - friendAge
+
+//compare
+age < friendAge
+height < friendHeight
+name == friendname
+hobby1 == friendHobby1
+hobby2 == friendHobby2
